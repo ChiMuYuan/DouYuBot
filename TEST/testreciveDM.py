8ca865a57841f32320ce2e4f0e81bb8b8e15ade3
ChiMuYuan <MySNN@outlook.com>
2018-03-16 22:06:04 +0800

    updata

--- TEST/testreciveDM.py
+++ TEST/testreciveDM.py
@@ -76,7 +76,8 @@ def showdm(data):
             nick.append(data[i])
         for i in range(tid + 5, eid):
             txt.append(data[i])
-        print(nick.decode("utf8") + " : " + txt.decode("utf8"))
+        if nick.decode("utf8") == "iiSNN":
+            print(nick.decode("utf8") + " : " + txt.decode("utf8"))
     elif cqid == 12:
         print("酬勤")
     # elif spid == 12:
@@ -109,7 +110,7 @@ Socket.connect((socket.gethostbyname("openbarrage.douyutv.com"), 8601))
 HOST_JSON = "[{\"ip\":\"119.90.49.90\",\"port\":\"8099\"},{\"ip\":\"119.90.49.106\",\"port\":\"8028\"},{\"ip\":\"119.90.49.87\",\"port\":\"8083\"},{\"ip\":\"119.90.49.105\",\"port\":\"8021\"},{\"ip\":\"119.90.49.104\",\"port\":\"8020\"},{\"ip\":\"119.90.49.86\",\"port\":\"8077\"},{\"ip\":\"119.90.49.86\",\"port\":\"8080\"},{\"ip\":\"119.90.49.89\",\"port\":\"8091\"},{\"ip\":\"119.90.49.91\",\"port\":\"8054\"},{\"ip\":\"119.90.49.103\",\"port\":\"8012\"},{\"ip\":\"119.90.49.102\",\"port\":\"8007\"},{\"ip\":\"119.90.49.107\",\"port\":\"8031\"},{\"ip\":\"119.90.49.88\",\"port\":\"8089\"},{\"ip\":\"119.90.49.110\",\"port\":\"8048\"},{\"ip\":\"119.90.49.101\",\"port\":\"8005\"},{\"ip\":\"119.90.49.103\",\"port\":\"8011\"},{\"ip\":\"119.90.49.110\",\"port\":\"8046\"},{\"ip\":\"119.90.49.104\",\"port\":\"8017\"},{\"ip\":\"119.90.49.89\",\"port\":\"8095\"},{\"ip\":\"119.90.49.105\",\"port\":\"8024\"},{\"ip\":\"119.90.49.86\",\"port\":\"8079\"},{\"ip\":\"119.90.49.86\",\"port\":\"8078\"},{\"ip\":\"119.90.49.109\",\"port\":\"8045\"},{\"ip\":\"119.90.49.95\",\"port\":\"8073\"},{\"ip\":\"119.90.49.103\",\"port\":\"8015\"},{\"ip\":\"119.90.49.104\",\"port\":\"8018\"},{\"ip\":\"119.90.49.107\",\"port\":\"8033\"},{\"ip\":\"119.90.49.92\",\"port\":\"8056\"},{\"ip\":\"119.90.49.93\",\"port\":\"8065\"},{\"ip\":\"119.90.49.92\",\"port\":\"8060\"}]";
 
 SocketSDM = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SocketSDM.connect((socket.gethostbyname("119.90.49.90"), 8099))
+SocketSDM.connect((socket.gethostbyname("119.90.49.89"), 8092))
 
 bbbbb = (socket.gethostbyname("119.90.49.89"), 8092)
 
@@ -125,7 +126,7 @@ md555.update(lasttt.encode("utf8"))
 vk = md555.hexdigest()
 a = "type@=loginreq/roomid@=74751/"
 b = "type@=joingroup/rid@=74751/gid@=-9999/"
-loginInfo = "type@=loginreq/username@=/ct@=0/password@=/roomid@=74751/devid@=" + uuiddd + "/rt@=" + timenow + "/vk@=" + vk + "/ver@=20150929/aver@=2017073111/ltkid@=/biz@=1/stk@=/"
+loginInfo = "type@=loginreq/username@=39019270/ct@=0/password@=/roomid@=74751/devid@=" + uuiddd + "/rt@=" + timenow + "/vk@=" + vk + "/ver@=20150929/aver@=2017073111/ltkid@=57277600/biz@=1/stk@=ca7624a66b58d4ef/"
 
 senddanmu = "type@=chatmessage/receiver@=0/content@=test/scope@=/col@=0/pid@=/p2p@=0/nc@=0/rev@=0/ifs@=0/"
 
@@ -133,15 +134,25 @@ ka = keepalive(Socket)
 rd = recivedata(Socket)
 
 SocketSDM.sendall(sendmeesage(loginInfo))
-print(SocketSDM.recv(10240).replace(b'@A', b'@').replace(b'@S', b'/'))
-print(SocketSDM.recv(10240).replace(b'@A', b'@').replace(b'@S', b'/'))
 
 rd.start()
 sss(a)
 sss(b)
 ka.start()
 
+class sdmsend(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        while True:
+            ssssss = input()
+            SocketSDM.sendall(sendmeesage(
+                "type@=chatmessage/receiver@=0/content@=" + ssssss + "/scope@=/col@=0/pid@=/p2p@=0/nc@=0/rev@=0/ifs@=0/"))
+
+sdmsss = sdmsend()
+sdmsss.start()
+
 while True:
-    ssssss = input()
-    SocketSDM.sendall(sendmeesage("type@=chatmessage/receiver@=0/content@=" + ssssss + "/scope@=/col@=0/pid@=/p2p@=0/nc@=0/rev@=0/ifs@=0/"))
     print(SocketSDM.recv(10240).replace(b'@A', b'@').replace(b'@S', b'/'))
+
--- TEST/ttttt.py
+++ TEST/ttttt.py
@@ -1,9 +1,6 @@
-from lxml import etree
+import socket
 
-a = "//*[@id=\"wp_news_w6\"]/table/tbody/tr[1]/td/table/tbody/tr/td[2]/a"
-t = open("C:/Users/ChiMu/Desktop/杂物/test/公告新闻.html", "r", encoding="utf8").read()
+S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+e = S.connect(("110.1.1.1", 10))
+print(e)
 
-html = etree.HTML(t)
-html_Data = html.xpath(a)
-print(html_Data)
-print(html_Data[0].text)
